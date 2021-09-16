24cb7589fd49f04a95785adef08f57b7e4622efa
aguilarkevin <kevinaguilar0813@gmail.com>
2021-09-16 16:43:28 -0600

    add: sales section in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <c-flex d="flex" flex-dir="column" justify-content="center" gap="14px">
-    <c-flex class="banner-container" flex-wrap="wrap">
+    <c-flex class="banner-container" flex-wrap="wrap" mt="4">
       <c-heading
         as="h1"
         size="2xl"
         font-size="44pt"
+        line-height="48pt"
         font-weight="light"
         text-transform="uppercase"
         >new</c-heading
       >
       <c-heading
         as="h1"
+        line-height="48pt"
+        d="inline-flex"
+        flex="wrap"
+        justify="center"
         size="2xl"
         display="inline"
         font-weight="light"
@@ -22,6 +27,7 @@
           font-size="12px"
           py="8px"
           px="14px"
+          mb="2"
           text-transform="uppercase"
           border-radius="full"
           color="#181818"
@@ -31,10 +37,16 @@
         sport collection
       </c-heading>
 
-      <c-flex w="100%" justify="space-between" align-items="end" gap="16px">
+      <c-flex
+        w="100%"
+        justify="space-between"
+        align-items="end"
+        gap="16px"
+        mt="8"
+      >
         <c-box flex="1">
           <c-image
-            h="240px"
+            h="220px"
             w="full"
             object-fit="cover"
             src="https://images.unsplash.com/photo-1551794840-8ae3b9c181f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
@@ -44,7 +56,7 @@
 
         <c-box flex="1">
           <c-image
-            h="200px"
+            h="180px"
             w="full"
             object-fit="cover"
             src="https://images.unsplash.com/photo-1546102597-4ac3be10560e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=313&q=80"
@@ -65,27 +77,132 @@
       </c-flex>
     </c-flex>
 
-    <c-stack>
-      <video v-chakra w="full" br="md" h="220px" mt="8" autoplay>
+    <c-stack position="relative" h="220px">
+      <video
+        v-chakra
+        position="relative"
+        w="full"
+        h="full"
+        br="md"
+        mt="8"
+        autoplay
+      >
         <source
           src="https://media.istockphoto.com/videos/man-dressing-up-video-id181842863"
           type="video/mp4"
         />
         Your browser does not support the video tag.
       </video>
-      <c-button font-weight="light">play video</c-button>
+      <c-button
+        position="absolute"
+        bottom="2"
+        left="10"
+        size="sm"
+        rounded="full"
+        bg="#1c1f22"
+        color="#FFFFFF"
+      >
+        <c-text font-size="10px" text-transform="uppercase">play video</c-text>
+      </c-button>
     </c-stack>
 
-    <c-flex class="sales-container">
+    <c-box class="sales-container" py="8">
       <c-heading
         as="h3"
         size="2xl"
         font-weight="light"
         text-transform="uppercase"
         font-size="44pt"
-        >sales</c-heading
+        mb="4"
       >
-    </c-flex>
+        sales
+      </c-heading>
+      <c-flex overflow="auto" gap="14px">
+        <c-stack>
+          <c-box h="320px" w="260px">
+            <c-image
+              h="full"
+              w="full"
+              object-fit="cover"
+              src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80"
+              alt="product"
+            />
+          </c-box>
+          <c-heading
+            as="h4"
+            font-size="10pt"
+            font-weight="regular"
+            m="0"
+            color="#eeeeee"
+            text-transform="uppercase"
+            >faloni's t-shirt</c-heading
+          >
+          <c-flex gap="4px">
+            <c-text color="#aaaaaa" text-decoration="line-through"
+              >$60.00</c-text
+            >
+            -
+            <c-text>$44.00</c-text>
+          </c-flex>
+        </c-stack>
+
+        <c-stack>
+          <c-box h="320px" w="260px">
+            <c-image
+              object-fit="cover"
+              h="full"
+              w="full"
+              src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80"
+              alt="product"
+            />
+          </c-box>
+          <c-heading
+            as="h4"
+            font-size="10pt"
+            font-weight="regular"
+            m="0"
+            color="#eeeeee"
+            text-transform="uppercase"
+            >faloni's t-shirt</c-heading
+          >
+          <c-flex gap="4px">
+            <c-text color="#aaaaaa" text-decoration="line-through"
+              >$60.00</c-text
+            >
+            -
+            <c-text>$44.00</c-text>
+          </c-flex>
+        </c-stack>
+
+        <c-stack>
+          <c-box h="320px" w="260px">
+            <c-image
+              h="full"
+              w="full"
+              object-fit="cover"
+              src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80"
+              alt="product"
+            />
+          </c-box>
+          <c-heading
+            as="h4"
+            font-size="10pt"
+            font-weight="regular"
+            m="0"
+            color="#eeeeee"
+            text-transform="uppercase"
+            >faloni's t-shirt</c-heading
+          >
+          <c-flex gap="4px">
+            <c-text color="#aaaaaa" text-decoration="line-through"
+              >$60.00</c-text
+            >
+            -
+            <c-text>$44.00</c-text>
+          </c-flex>
+        </c-stack>
+      </c-flex>
+    </c-box>
   </c-flex>
 </template>
 
