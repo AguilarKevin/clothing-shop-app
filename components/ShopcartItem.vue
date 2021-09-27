b17c93772f10873a5832d189533cae0e6ed3d3ec
aguilarkevin <11aazx1190@gmail.com>
2021-09-27 16:59:49 -0600

    improve: shopcart page

--- components/ShopcartItem.vue
+++ components/ShopcartItem.vue
@@ -39,8 +39,8 @@ export default {
     <c-box w="30%" h="full">
       <c-image w="full" h="full" object-fit="cover" :src="img.url" />
     </c-box>
-    <c-stack>
-      <c-flex gap="14px">
+    <c-flex h="full" flex-dir="column" justify="start" gap="10px">
+      <c-flex gap="14px" justify="space-between">
         <c-heading
           font-weight="regular"
           :align="['center', 'left']"
@@ -51,50 +51,60 @@ export default {
         <c-icon-button size="xs" variant="outline" icon="small-close" />
       </c-flex>
 
-      <c-flex gap="4px">
-        <c-box rounded="md" p="4px" w="44px" h="24px" border="1px"
-          ><c-box
-            rounded="md"
-            border="2px"
-            border-color="gray.800"
-            w="full"
-            h="full"
-            :bg="color"
-          />
-        </c-box>
+      <c-flex gap="4px" justify="space-between">
+        <c-flex gap="4px">
+          <c-text>Subtotal: </c-text>
+          <c-box rounded="md" p="4px" w="44px" h="24px" border="1px"
+            ><c-box
+              rounded="md"
+              border="2px"
+              border-color="gray.800"
+              w="full"
+              h="full"
+              :bg="color"
+            />
+          </c-box>
+        </c-flex>
 
+        <c-divider border-color="white" orientation="vertical" />
+        <c-flex gap="4px">
+          <c-text>Size: </c-text>
+          <c-flex
+            align="center"
+            justify="center"
+            rounded="md"
+            w="44px"
+            h="24px"
+            border="1px"
+          >
+            <c-text font-size="11px" text-transform="uppercase">
+              {{ size }}
+            </c-text>
+          </c-flex></c-flex
+        >
+      </c-flex>
+      <c-flex gap="8px">
+        <c-text>Quantity: </c-text>
         <c-flex
           align="center"
-          justify="center"
+          justify="space-between"
+          gap="4px"
           rounded="md"
-          w="44px"
           h="24px"
           border="1px"
         >
-          <c-text font-size="11px" text-transform="uppercase">
-            {{ size }}
-          </c-text>
+          <c-button h="24px" variant="ghost" @click="decrementCount(id)">
+            -
+          </c-button>
+          <c-text px="28px" align="center" font-size="12px">{{
+            productCount
+          }}</c-text>
+          <c-button h="24px" variant="ghost" @click="incrementCount(id)">
+            +
+          </c-button>
         </c-flex>
       </c-flex>
-      <c-flex
-        align="center"
-        justify="space-between"
-        gap="4px"
-        rounded="md"
-        h="24px"
-        border="1px"
-      >
-        <c-button h="24px" variant="ghost" @click="decrementCount(id)">
-          -
-        </c-button>
-        <c-text px="28px" align="center" font-size="12px">{{
-          productCount
-        }}</c-text>
-        <c-button h="24px" variant="ghost" @click="incrementCount(id)">
-          +
-        </c-button>
-      </c-flex>
-    </c-stack>
+    </c-flex>
   </c-flex>
 </template>
 
--- pages/shopcart.vue
+++ pages/shopcart.vue
@@ -1,16 +1,33 @@
 <template>
   <div v-chakra h="82vh">
     <c-flex v-if="shopcart.length > 0" flex-dir="column" gap="14px">
-      <shopcart-item
-        v-for="item in shopcart"
-        :id="item.id"
-        :key="item.id"
-        :img="item.imgSrc"
-        :count="item.count"
-        :title="item.title"
-        :color="item.color"
-        :size="item.size"
-      />
+      <c-heading
+        as="h1"
+        :font-size="['24pt', '44pt']"
+        text-transform="uppercase"
+        :line-height="['24pt', '44pt']"
+        align="center"
+        py="24px"
+        >Item List</c-heading
+      >
+      <div>
+        <shopcart-item
+          v-for="item in shopcart"
+          :id="item.id"
+          :key="item.id"
+          :img="item.imgSrc"
+          :count="item.count"
+          :title="item.title"
+          :color="item.color"
+          :size="item.size"
+        />
+        <c-divider border-color="white" />
+      </div>
+      <c-flex>
+        <c-text>Subtotal</c-text>
+        <c-divider border-color="white" orientation="vertical" />
+        <c-text>{{ shopcart.length }} items</c-text>
+      </c-flex>
     </c-flex>
     <c-flex v-else h="100%" flex-dir="column" justify="center" align="center">
       <iframe src="https://embed.lottiefiles.com/animation/9091"></iframe>
